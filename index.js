9405ba49fc12289d07407d6cd6a0322b0a2f54f7
lincolnlim80 <mypropertyagent88@gmail.com>
2023-06-20 00:09:59 +0800

    firstupload

---
+++ index.js
@@ -0,0 +1,8 @@
+function showGetInTouchSales() {
+    document.getElementById("popupFormSales").style.display = "block";
+  }
+
+function closeFormSales() {
+    document.getElementById("popupFormSales").style.display = "none";
+  }
+
